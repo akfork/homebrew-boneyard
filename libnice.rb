e7888889168e85d4c460bc171dd00d78863976be
Akagi201 <akagi201@gmail.com>
2017-03-23 11:43:30 +0800

    feat: update libnice to version 0.1.13

--- libnice.rb
+++ libnice.rb
@@ -1,8 +1,8 @@
 class Libnice < Formula
   desc "GLib ICE implementation"
   homepage "https://wiki.freedesktop.org/nice/"
-  url "https://nice.freedesktop.org/releases/libnice-0.1.7.tar.gz"
-  sha256 "4ed165aa2203136dce548c7cef735d8becf5d9869793f96b99dcbbaa9acf78d8"
+  url "https://nice.freedesktop.org/releases/libnice-0.1.13.tar.gz"
+  sha256 "61112d9f3be933a827c8365f20551563953af6718057928f51f487bfe88419e1"
 
   depends_on "pkg-config" => :build
   depends_on "glib"
